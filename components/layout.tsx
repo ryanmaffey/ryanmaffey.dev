56b6f6f486dc6cb6b582b7adecaf4ee520658aa9
Ryan Maffey <ryan.maffey@gmail.com>
2020-08-27 17:43:27 +0100

    fix(meta): twitter card

--- components/layout.tsx
+++ components/layout.tsx
@@ -26,7 +26,7 @@ export const Layout: React.FunctionComponent = ({ children }) => {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         /> */}
                 <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content={description} />
+                <meta name="twitter:card" content="summary" />
                 <meta name="twitter:creator" content="@ryanmaffey" />
             </Head>
             <div
