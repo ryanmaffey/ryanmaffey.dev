14ecbf211688f743949b256ac7f2434bfe2734b8
Ryan Maffey <ryan.maffey@gmail.com>
2020-08-28 09:41:07 +0100

    add icons and theme

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,8 +11,33 @@ export const Layout: React.FunctionComponent<{
     return (
         <>
             <Head>
-                {/* Icons */}
-                <link rel="icon" href="/favicon.ico" />
+                {/* Icons and Theme */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png?v=1"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png?v=1"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png?v=1"
+                />
+                <link rel="manifest" href="/site.webmanifest?v=1" />
+                <link
+                    rel="mask-icon"
+                    href="/safari-pinned-tab.svg?v=1"
+                    color="#2e3748"
+                />
+                <link rel="shortcut icon" href="/favicon.ico?v=1" />
+                <meta name="msapplication-TileColor" content="#f5ad5d" />
+                <meta name="theme-color" content="#f5ad5d" />
 
                 {/* Meta */}
                 <meta name="description" content={props.description} />
